78f46a5d17a38c9e3f716bcdd2a186fd661983c9
DominikNITA <36695471+DominikNITA@users.noreply.github.com>
2021-03-24 20:10:30 +0100

    Add express library support

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,22 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 
 const hostname = 'localhost';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
 
-server.listen(port, hostname, () => {
+app.use(express.json())
+
+app.get('/', (req,res) => {
+  res.send('Hello World');
+})
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World');
+// });
+
+
+app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
