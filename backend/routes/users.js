df8178b45510b0e493f6a413b3e859db3c82e69a
bubsi-91 <66141310+bubsi-91@users.noreply.github.com>
2021-04-09 17:49:15 +0200

    Add User management

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
+const User = require ("../models/user");
 const UserService = require("../services/UserService");
 
 /** 
@@ -33,13 +34,143 @@ const UserService = require("../services/UserService");
  *                  description: User's last name
  */
 
-router.get("/", async (req, res, next) => {
-    try {
-        // a faire
-        let result = await UserService.getAllUsers();
-    } catch (err) {
-        next(err);
+
+/**
+ *
+ * @swagger
+ * /events/:
+ *   get:
+ *     tags: [Users]
+ *     summary: Get all users
+ *     responses:
+ *       200:
+ *         description: Returns all users
+ *         content:
+ *          application/json:
+ *              schema:
+ *                  type: array
+ *                  items:
+ *                      $ref: '#/components/schemas/User'
+ */
+router.get("/", async (req, res) => {
+    res.json(await UserService.getAllUsers());
+});
+
+/**
+ * @swagger
+ * /users/{id}:
+ *  get:
+ *      tags: [Users]
+ *      summary: Get user by id
+ *      responses:
+ *          200:
+ *              description: OK
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/User'
+ *          400:
+ *              description: Authorization error
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/User'
+ *      parameters:
+ *          - in: path
+ *            name: id
+ *            schema:
+ *              type: integer
+ *            required: true
+ *            description: Numeric id of the user to get
+ */
+ router.get("/:id", async (req, res) => {
+    console.log(req.params.id);
+    const user = await EventService.getUserbById(req.params.id);
+    if (user == null) {
+        res.statusCode = 400;
+        res.statusMessage = "Invalid user id";
+        res.end();
+        return;
+    } else {
+        res.json(user);
     }
+});
+
+
+/**
+ * @swagger
+ * /users/:
+ *  post:
+ *      tags: [Users]
+ *      summary: Creates new user
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      $ref: '#/components/schemas/User'
+ *      responses:
+ *          200:
+ *              description: OK
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/User'
+ *          400:
+ *              description: User creation error
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/User'
+ */
+ router.post("/", async (req, res) => {
+    if (req.body.user == null) {
+        res.statusCode = 400;
+        res.statusMessage = "Failed to create user";
+        res.end();
+        return;
+    }
+    const user = await UserService.addUser(req.body.user);
+    res.json(user);
+});
+
+/**
+ * @swagger
+ * /users/{Id}:
+ *  post:
+ *      tags: [Users]
+ *      summary: Anonymise user
+ *      responses:
+ *          200:
+ *              description: OK
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/User'
+ *          400:
+ *              description: User anonymation error
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/User'
+ *      parameters:
+ *          - in: path
+ *            name: id
+ *            schema:
+ *              type: integer
+ *            required: true
+ *            description: Numeric id of the user to anonymise
+ */
+ router.post("/:id", async (req, res) => {
+    console.log(req.params.id);
+    const user = await UserService.anonymizeUser(req.params.id);
+    if (user == null) {
+        res.statusCode = 400;
+        res.statusMessage = "Can't find user id";
+        res.end();
+        return;
+    }
+    res.json(user);
 });
 
 module.exports = router;
