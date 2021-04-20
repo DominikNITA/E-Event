d857caada23994dfe51620baf8bbb1ae250acd21
DominikNITA <36695471+DominikNITA@users.noreply.github.com>
2021-04-20 23:59:22 +0200

    Add connection to backend helper site + change to auth middleware

--- backend/utility/Middlewares.js
+++ backend/utility/Middlewares.js
@@ -8,20 +8,23 @@ async function authenticateToken(req, res, next) {
     try {
         const authHeader = req.headers["auth"];
         const token = authHeader; // && authHeader.split(' ')[1] //uncomment when using Bearer token
-        if (token == null) {
-            //throw new ErrorResponse(ErrorResponse.unauthorizedStatusCode, "Auth token not passed");
+        if (token == null || token === "" || token === "null") {
             console.log("AuthToken not passed. TODO: uncomment error section");
             return next();
         }
+        let userFromToken;
+        try {
+            userFromToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        } catch {
+            throw new ErrorResponse(ErrorResponse.forbiddenStatusCode, "Problem with token");
+        }
+        const user = await UserService.getUserById(userFromToken.userId);
+        if (user == null)
+            throw new ErrorResponse(ErrorResponse.badRequestStatusCode, "Token signed for not existing user");
 
-        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, async (err, userFromToken) => {
-            if (err) throw new ErrorResponse(ErrorResponse.forbiddenStatusCode, "Problem with token");
-            const user = await UserService.getUserById(userFromToken.userId);
-            if (user == null)
-                throw new ErrorResponse(ErrorResponse.badRequestStatusCode, "Token signed for not existing user");
-            req.user = user;
-            next();
-        });
+        req.user = user;
+
+        next();
     } catch (err) {
         next(err);
     }
