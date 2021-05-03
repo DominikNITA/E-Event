bb210231932492dae6241fd4c96916060900566f
DominikNITA <36695471+DominikNITA@users.noreply.github.com>
2021-05-03 14:42:31 +0200

    Implement registration

--- eeventfrontend/src/views/Register.vue
+++ eeventfrontend/src/views/Register.vue
@@ -5,19 +5,54 @@
     </div>
     <div class="form-container">
       <div class="form-inner">
-        <form action="{% url 'connexion' %}" class="signup">
+        <form class="signup">
+          <div v-if="error != null" class="alert alert-danger">{{ error }}</div>
           <div class="field">
-            <input type="text" placeholder="Email Address" required />
+            <input
+              type="text"
+              placeholder="Email Address"
+              v-model="email"
+              required
+            />
           </div>
           <div class="field">
-            <input type="password" placeholder="Password" required />
+            <input
+              type="text"
+              placeholder="First Name"
+              v-model="firstName"
+              required
+            />
           </div>
           <div class="field">
-            <input type="password" placeholder="Confirm password" required />
+            <input
+              type="text"
+              placeholder="Last Name"
+              v-model="lastName"
+              required
+            />
+          </div>
+          <div class="field">
+            <input type="text" placeholder="Nick" v-model="nick" required />
+          </div>
+          <div class="field">
+            <input
+              type="password"
+              placeholder="Password"
+              v-model="password"
+              required
+            />
+          </div>
+          <div class="field">
+            <input
+              type="password"
+              placeholder="Confirm password"
+              v-model="passwordConfirm"
+              required
+            />
           </div>
           <div class="field btn">
             <div class="btn-layer"></div>
-            <input type="submit" value="Signup" />
+            <input type="submit" value="Signup" @click="register" />
           </div>
           <div class="signup-link">
             Already have an account?
@@ -31,7 +66,46 @@
 
 <script>
 import "@/assets/styles/authViews.css";
-export default {};
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      email: "",
+      firstName: "",
+      lastName: "",
+      nick: "",
+      password: "",
+      passwordConfirm: "",
+      error: null,
+    };
+  },
+  methods: {
+    register() {
+      this.error = null;
+      if (this.password !== this.passwordConfirm) {
+        this.error = "Passwords do not match!";
+        return;
+      }
+
+      axios
+        .post(`${process.env.VUE_APP_BACKEND_ADDRESS}/auth/register`, {
+          user: {
+            email: this.email,
+            firstName: this.firstName,
+            lastName: this.lastName,
+            nick: this.nick,
+          },
+          password: this.password,
+        })
+        .then((response) => {
+          console.log(response.data);
+          this.$router.push({ name: "Login" });
+        })
+        .catch((err) => (this.error = err.response.data));
+    },
+  },
+};
 </script>
 
 <style>
