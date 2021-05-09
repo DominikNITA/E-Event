ac2c7f19a3a77dc24d401f42011cf6cc659d7041
DominikNITA <36695471+DominikNITA@users.noreply.github.com>
2021-05-09 23:24:08 +0200

    Fix home not showing

--- eeventfrontend/src/views/Home.vue
+++ eeventfrontend/src/views/Home.vue
@@ -3,44 +3,35 @@
   <section>
     <search-event-bar @searchEvent="searchEvent"></search-event-bar>
     <h1 v-if="search != null">Recherche d'événements avec "{{ search }}"</h1>
-    <h1 v-else>Prochains événements</h1> <br>
+    <h1 v-else>Prochains événements</h1>
+    <br />
     <div v-for="event in events" v-bind:key="event.id">
-      
       <div class="container-fluid" v-if="event != null">
-      <div class="container">
-        <div class="row">
+        <div class="container">
+          <div class="row">
+            <!-- carré avec le centre d'intérêt de l'événement -->
+            <div class="circle" v-if="event.categories.length > 0">
+              {{ event.categories[0].title }}
+            </div>
 
-          <!-- carré avec le centre d'intérêt de l'événement -->
-          <div class="circle">
-          {{event.categories[0].title}}
+            <div class="col-8">
+              <br /><br />
+              <big
+                >{{ event.name }} à {{ event.place.place_name }},
+                {{ event.place.address }}</big
+              >
+            </div>
+
+            <div class="col-2">
+              <br /><br />
+              <router-link
+                :to="{ name: 'Event Details', params: { eventId: event.id } }"
+                ><button>Plus d'infos</button></router-link
+              >
+            </div>
           </div>
-
-          <div class="col-8">
-            <br><br>
-            <big>{{ event.name }}  à {{ event.place.place_name }}, {{ event.place.address }}</big>
-
-          </div>
-
-          <div class="col-2">
-            <br><br>
-            <router-link
-            :to="{ name: 'Event Details', params: { eventId: event.id } }"
-            ><button>Plus d'infos</button></router-link
-            > 
-          </div>
-
-
-          
-
         </div>
       </div>
-      </div>
-      
-      
-      
-      
-      
-      
     </div>
   </section>
 </template>
@@ -108,19 +99,18 @@ export default {
 </script>
 
 <style>
-body{
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-
+body {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
 }
 
-.circle{
-    margin:10px;
-    width:100px;
-    background:white;
-    height:100px;
-    text-align:center;
-    border-radius:100px;
-    line-height:30px;
-    padding-top:30px;
+.circle {
+  margin: 10px;
+  width: 100px;
+  background: white;
+  height: 100px;
+  text-align: center;
+  border-radius: 100px;
+  line-height: 30px;
+  padding-top: 30px;
 }
 </style>
