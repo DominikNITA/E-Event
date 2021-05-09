8a4a986dcab51e8a8c257e57001b4b7b47c95c8b
bubsi-91 <66141310+bubsi-91@users.noreply.github.com>
2021-05-10 00:30:31 +0200

    remove About page

--- eeventfrontend/src/router/index.js
+++ eeventfrontend/src/router/index.js
@@ -25,14 +25,6 @@ const routes = [
         component: () => import("../views/Register.vue"),
     },
     {
-        path: "/about",
-        name: "About",
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ "../views/About.vue"),
-    },
-    {
         path: "/events/:eventId",
         name: "Event Details",
         component: () => import("../views/EventDetails.vue"),
